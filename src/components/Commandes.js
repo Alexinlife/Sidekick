d036883f13594815d33ce5affe153f165e01615c
Alexinlife <alexinlife.exe@gmail.com>
2021-01-26 20:45:04 -0500

    Premier commit

---
+++ src/components/Commandes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../css/Style.css';
+
+
+class Commandes extends React.Component {
+
+  render() {
+
+    return (
+      <div>
+        <header>
+
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Commandes;
