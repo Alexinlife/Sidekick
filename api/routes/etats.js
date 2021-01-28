8fd739220a02218416a5d223bf6d6c80f0bb0486
Alexinlife <alexinlife.exe@gmail.com>
2021-01-28 16:43:39 -0500

    squelette des méthodes pour états et produits

--- api/routes/etats.js
+++ api/routes/etats.js
@@ -5,11 +5,24 @@ var router = express.Router();
 // Créer un état
 router.post('/create', async (req, res) => {
     try {
-        const { commande_id, texte } = req.body
-        const nouvCommande = await pool.query(
-            'INSERT INTO commandes (commande_id, texte, date) VALUES ($1, $2)',
-            [commande_id, texte]
-            );
+
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+// Afficher l'état le plus récent d'une commande
+router.get('/:commande_id', async (req, res) => {
+    try {
+
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+// Afficher l'historique d'état d'une commande
+router.get('/:commande_id', async (req, res) => {
+    try {
 
     } catch (error) {
         console.log(error.message);
--- api/routes/produits.js
+++ api/routes/produits.js
@@ -2,8 +2,40 @@ const express = require('express');
 const pool = require('../db');
 var router = express.Router();
 
-router.get('/', (req, res) => {
+// Créer un produit
+router.post('/create', async (req, res) => {
+    try {
 
+    } catch (error) {
+        console.log(error.message);
+    }
 });
+
+// Afficher tous les produits d'une commande
+router.get('/:commande_id', async (req, res) => {
+    try {
+
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+// Modifier un produit
+router.put('/:id', async (req, res) => {
+    try {
+
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+// Supprimer un produit
+router.delete('/:id', async (req, res) => {
+    try {
+
+    } catch (error) {
+        console.log(error.message);
+    }
+})
 
 module.exports = router;
