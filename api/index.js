c4f2d92b64b0e16d63175b672e37b617b3832b4c
Alexinlife <alexinlife.exe@gmail.com>
2021-01-29 16:43:40 -0500

    exemples nodemailer pour la semaine prochaine

--- api/index.js
+++ api/index.js
@@ -1,10 +1,37 @@
 const express = require('express');
+const nodemailer = require('nodemailer');
 const pool = require('./db');
 
 // Fichiers de routes à part
 var commandes = require('./routes/commandes');
 var produits = require('./routes/produits');
 var etats = require('./routes/etats');
+
+// Nodemailer
+const transporter = nodemailer.createTransport({
+    port: 465,               // true for 465, false for other ports
+    host: "smtp.gmail.com",
+    auth: {
+        user: 'youremail@gmail.com',
+        pass: 'password',
+    },
+    secure: true,
+});
+
+const mailData = {
+    from: 'youremail@gmail.com',  // sender address
+    to: 'myfriend@gmail.com',   // list of receivers
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!',
+    html: '<b>Hey there! </b><br> This is our first message sent with Nodemailer < br />',
+};
+
+/*transporter.sendMail(mailOptions, function (err, info) {
+    if (err)
+        console.log(err)
+    else
+        console.log(info);
+});*/
 
 const app = express();
 app.use(express.json());
