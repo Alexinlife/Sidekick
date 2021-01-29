f49fa16199b13e193c8269ab1db4dcbbae49dea0
Alexinlife <alexinlife.exe@gmail.com>
2021-01-29 15:09:35 -0500

    méthodes pour produits et etats (sans validation)

--- api/routes/etats.js
+++ api/routes/etats.js
@@ -3,18 +3,31 @@ const pool = require('../db');
 var router = express.Router();
 
 // Créer un état
-router.post('/create', async (req, res) => {
+router.post('/create/:commande_id', async (req, res) => {
     try {
-
+        const { commande_id } = req.params;
+        const { texte } = req.body;
+        // Requête
+        const nouvProduit = await pool.query("INSERT INTO etats (texte, commande_id) VALUES ($1, $2) RETURNING *",
+            [texte, commande_id]);
+        // Succès
+        res.json(nouvProduit.rows);
+        console.log("Success POST.");
     } catch (error) {
         console.log(error.message);
     }
 });
 
 // Afficher l'état le plus récent d'une commande
-router.get('/:commande_id', async (req, res) => {
+router.get('/last/:commande_id', async (req, res) => {
     try {
-
+        const { commande_id } = req.params;
+        // Requête
+        const etat = await pool.query("SELECT texte, date FROM etats WHERE commande_id = $1 ORDER BY date DESC LIMIT 1",
+            [commande_id]);
+        // Succès
+        res.json(etat.rows);
+        console.log("Success GET by Order_ID.");
     } catch (error) {
         console.log(error.message);
     }
@@ -23,7 +36,40 @@ router.get('/:commande_id', async (req, res) => {
 // Afficher l'historique d'état d'une commande
 router.get('/:commande_id', async (req, res) => {
     try {
+        const { commande_id } = req.params;
+        // Requête
+        const etats = await pool.query("SELECT texte, date FROM etats WHERE commande_id = $1",
+            [commande_id]);
+        // Succès
+        res.json(etats.rows);
+        console.log("Success GET by Order_ID.");
+    } catch (error) {
+        console.log(error.message);
+    }
+});
 
+// Afficher tous les états
+router.get('/', async (req, res) => {
+    try {
+        // Requête
+        const etats = await pool.query("SELECT texte, date FROM etats");
+        // Succès
+        res.json(etats.rows);
+        console.log("Success GET by Order_ID.");
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+// Supprimer tous les états d'une commande
+router.delete('/:commande_id', async (req, res) => {
+    try {
+        const { commande_id } = req.params;
+        // Requête
+        const supprEtats = await pool.query("DELETE FROM etats WHERE commande_id = $1 RETURNING *", [commande_id]);
+        // Succès
+        res.json(supprEtats.rows);
+        console.log("Success DELETE.");
     } catch (error) {
         console.log(error.message);
     }
--- api/routes/produits.js
+++ api/routes/produits.js
@@ -3,9 +3,16 @@ const pool = require('../db');
 var router = express.Router();
 
 // Créer un produit
-router.post('/create', async (req, res) => {
+router.post('/create/:commande_id', async (req, res) => {
     try {
-
+        const { commande_id } = req.params;
+        const { code, description, qte_demandee, prix } = req.body;
+        // Requête
+        const nouvProduit = await pool.query("INSERT INTO produits (code, description, qte_demandee, prix, commande_id) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+            [code, description, qte_demandee, prix, commande_id]);
+        // Succès
+        res.json(nouvProduit.rows);
+        console.log("Success POST.");
     } catch (error) {
         console.log(error.message);
     }
@@ -14,7 +21,13 @@ router.post('/create', async (req, res) => {
 // Afficher tous les produits d'une commande
 router.get('/:commande_id', async (req, res) => {
     try {
-
+        const { commande_id } = req.params;
+        // Requête
+        const produits = await pool.query("SELECT code, description, qte_demandee, qte_recueillie, prix FROM produits WHERE commande_id = $1",
+            [commande_id]);
+        // Succès
+        res.json(produits.rows);
+        console.log("Success GET by Order_ID.");
     } catch (error) {
         console.log(error.message);
     }
@@ -23,7 +36,14 @@ router.get('/:commande_id', async (req, res) => {
 // Modifier un produit
 router.put('/:id', async (req, res) => {
     try {
-
+        const { id } = req.params;
+        const { code, description, qte_demandee, prix } = req.body;
+        // Requête
+        const modProduit = await pool.query("UPDATE produits SET code = $1, description = $2, qte_demandee = $3, prix = $4 WHERE id = $5 RETURNING *",
+            [code, description, qte_demandee, prix, id]);
+        // Succès
+        res.json(modProduit.rows);
+        console.log("Succes PUT.");
     } catch (error) {
         console.log(error.message);
     }
@@ -32,10 +52,29 @@ router.put('/:id', async (req, res) => {
 // Supprimer un produit
 router.delete('/:id', async (req, res) => {
     try {
-
+        const { id } = req.params;
+        // Requête
+        const supprProduit = await pool.query("DELETE FROM produits WHERE id = $1 RETURNING *", [id]);
+        // Succès
+        res.json(supprProduit.rows);
+        console.log("Success DELETE.");
     } catch (error) {
         console.log(error.message);
     }
-})
+});
+
+// Supprimer tous les produits d'une commande
+router.delete('/:commande_id', async (req, res) => {
+    try {
+        const { commande_id } = req.params;
+        // Requête
+        const supprProduits = await pool.query("DELETE FROM produits WHERE commande_id = $1 RETURNING *", [commande_id]);
+        // Succès
+        res.json(supprProduits.rows);
+        console.log("Success DELETE.");
+    } catch (error) {
+        console.log(error.message);
+    }
+});
 
 module.exports = router;
