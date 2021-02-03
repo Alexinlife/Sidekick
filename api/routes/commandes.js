123c8a18918b1cbb06e979aebefc2f4c6083f15e
Alexinlife <alexinlife.exe@gmail.com>
2021-02-02 21:46:44 -0500

    maj de la validation des commandes avec Joi

--- api/routes/commandes.js
+++ api/routes/commandes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Joi = require('joi');
 const nodemailer = require('nodemailer');
 const pool = require('../db');
 var router = express.Router();
@@ -15,7 +16,9 @@ const transporter = nodemailer.createTransport({
 });
 
 /**
- * Compose un message avant de l'envoyer via Nodemailer
+ * @author Alex Lajeunesse
+ * @function sendMail
+ * @description Compose un message avant de l'envoyer via Nodemailer
  * @param {*} attention La personne devant recevoir le message
  * @param {*} subject Le sujet du message
  * @param {*} text Le texte contenu dans le message
@@ -37,44 +40,121 @@ function sendMail(attention, subject, text) {
     });
 }
 
+// Validation Joi pour les champs
+const schema = Joi.object({
+    entreprise: Joi.string()
+        .allow("")
+        .max(64),
+
+    nom: Joi.string()
+        .allow("")
+        .max(64),
+
+    no_compte: Joi.string()
+        .max(9)
+        .pattern(/^\d+$/),
+
+    telephone: Joi.string()
+        .allow("")
+        .max(32),
+
+    courriel: Joi.string()
+        .email()
+        .max(255),
+
+    po_client: Joi.string()
+        .allow("")
+        .max(64),
+
+    vendeur: Joi.string()
+        .max(4)
+        .required(),
+
+    commentaire: Joi.string()
+        .allow("")
+        .max(512),
+
+    attention: Joi.string()
+        .email()
+        .allow("")
+        .max(255)
+        .required()
+})
+    .or('entreprise', 'nom')
+    .or('entreprise', 'telephone', 'courriel');
+
+// Validation Joi pour l'identifiant
+const schemaForId = Joi.object({
+    id: Joi.number()
+        .integer()
+        .positive()
+        .required()
+});
+
+/**
+ * @author Alex Lajeunesse
+ * @function validateCommande
+ * @description Valide les paramètres entrés avec Joi
+ * @param {*} entreprise Le nom de l'entreprise à valider
+ * @param {*} nom Le nom du client à valider
+ * @param {*} no_compte Le numéro du compte à valider
+ * @param {*} telephone Le téléphone du client à valider
+ * @param {*} courriel L'adresse courriel du client à valider
+ * @param {*} po_client Le numéro de commande du client à valider
+ * @param {*} vendeur Les initiales du vendeur à valider
+ * @param {*} commentaire Le commentaire de la commande à valider
+ * @param {*} attention Le courriel auquel envoyer la notification à valider
+ */
+async function validateCommande(entreprise, nom, no_compte, telephone, courriel, po_client, vendeur, commentaire, attention) {
+    try {
+        const validation = await schema.validateAsync({
+            entreprise: entreprise,
+            nom: nom,
+            no_compte: no_compte,
+            telephone: telephone,
+            courriel: courriel,
+            po_client: po_client,
+            vendeur: vendeur,
+            commentaire: commentaire,
+            attention: attention
+        });
+        // Succès
+        console.log(validation);
+        return true;
+        // Erreur
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
+
+/**
+ * @author Alex Lajeunesse
+ * @function validateId
+ * @description Valide l'identifiant avec Joi
+ * @param {*} id L'identifiant à valider
+ */
+async function validateId(id) {
+    try {
+        const validation = await schemaForId.validateAsync({
+            id: id
+        });
+        // Succès
+        console.log(validation);
+        return true;
+        // Erreur
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
+
 // Créer une commande
 router.post('/create', async (req, res) => {
     try {
         const { entreprise, nom, no_compte, telephone, courriel, po_client, vendeur, commentaire, attention } = req.body;
-        // Champs obligatoires
-        if (vendeur == null || attention == null) {
-            res.status(400).json({ "erreur": "Données invalides", "code": 1 });
-        }
-        // Entreprise et nom vides
-        if (entreprise == null && nom == null) {
-            res.status(400).json({ "erreur": "Données invalides", "code": 2 });
-        }
-        // Entreprise, telephone, et courriel vides
-        if (entreprise == null && telephone == null && courriel == null) {
-            res.status(400).json({ "erreur": "Données invalides", "code": 3 });
-        }
-        // Validation type champs, s'il y a lieu
-        if ((entreprise && typeof entreprise != "string") || (nom && typeof nom != "string") || (no_compte && typeof no_compte != "number") ||
-            (telephone && typeof telephone != "string") || (courriel && typeof courriel != "string") || (po_client && typeof po_client != "string") ||
-            (vendeur && typeof vendeur != "string") || (commentaire && typeof commentaire != "string") || (attention && typeof attention != "string")) {
-            res.status(400).json({ "erreur": "Données invalides", "code": 4 });
-        }
-        // Validation longueur champs
-        if ((entreprise && entreprise.length > 64) || (nom && nom.length > 64) || (no_compte && ((no_compte.toString().length > 9) || no_compte < 0 || !(Number.isInteger(no_compte)) ||
-            no_compte > Number.MAX_SAFE_INTEGER)) || (telephone && telephone.length > 32) || (courriel && courriel.length > 255) || (po_client && po_client.length > 64) ||
-            (vendeur && vendeur.length > 4) || (commentaire && commentaire.length > 512) || (attention && attention.length > 255)) {
-            res.status(400).json({ "erreur": "Données invalides", "code": 5 });
-        }
-        /**
-         * Validation adresse courriel valide
-         * @see https://www.w3resource.com/javascript/form/email-validation.php
-         */
-        if (!(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(courriel))) {
-            res.status(400).json({ "erreur": "Données invalides", "code": 5 });
-        }
-        // TODO: Utiliser cette librairie pour une validation plus clean
         // Requête
-        else {
+        if (await validateCommande(entreprise, nom, no_compte, telephone, courriel, po_client, vendeur, commentaire, attention)) {
             const nouvCommande = await pool.query(
                 "INSERT INTO commandes (entreprise, nom, no_compte, telephone, courriel, po_client, vendeur, commentaire, attention) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *",
                 [entreprise, nom, no_compte, telephone, courriel, po_client, vendeur, commentaire, attention]
@@ -87,6 +167,9 @@ router.post('/create', async (req, res) => {
             res.json([nouvCommande.rows, nouvEtat.rows]);
             sendMail(attention, "Nouvelle commande", "Une nouvelle commande vous est assignée dans l\'application Sidekick.");
             console.log("Success POST.");
+            // Ereur
+        } else {
+            res.status(400).json({ "erreur": "Données invalides" });
         }
     } catch (error) {
         console.log(error.message);
@@ -101,6 +184,7 @@ router.get('/', async (req, res) => {
         // Succès
         res.json(commandes.rows);
         console.log("Success GET.");
+        // Erreur
     } catch (error) {
         console.log(error.message);
     }
@@ -115,6 +199,7 @@ router.get('/:id', async (req, res) => {
         // Succès
         res.json(commande.rows);
         console.log("Success GET by ID.");
+        // Erreur
     } catch (error) {
         console.log(error.message);
     }
@@ -125,39 +210,8 @@ router.put('/:id', async (req, res) => {
     try {
         const { id } = req.params;
         const { entreprise, nom, no_compte, telephone, courriel, po_client, vendeur, commentaire, attention } = req.body;
-        // Champs obligatoires
-        if (vendeur == null || attention == null) {
-            res.status(400).json({ "erreur": "Données invalides", "code": 1 });
-        }
-        // Entreprise et nom vides
-        if (entreprise == null && nom == null) {
-            res.status(400).json({ "erreur": "Données invalides", "code": 2 });
-        }
-        // Entreprise, telephone, et courriel vides
-        if (entreprise == null && telephone == null && courriel == null) {
-            res.status(400).json({ "erreur": "Données invalides", "code": 3 });
-        }
-        // Validation type champs, s'il y a lieu
-        if ((entreprise && typeof entreprise != "string") || (nom && typeof nom != "string") || (no_compte && typeof no_compte != "number") ||
-            (telephone && typeof telephone != "string") || (courriel && typeof courriel != "string") || (po_client && typeof po_client != "string") ||
-            (vendeur && typeof vendeur != "string") || (commentaire && typeof commentaire != "string") || (attention && typeof attention != "string")) {
-            res.status(400).json({ "erreur": "Données invalides", "code": 4 });
-        }
-        // Validation longueur champs
-        if ((entreprise && entreprise.length > 64) || (nom && nom.length > 64) || (no_compte && ((no_compte.toString().length > 9) || no_compte < 0 || !(Number.isInteger(no_compte)) ||
-            no_compte > Number.MAX_SAFE_INTEGER)) || (telephone && telephone.length > 32) || (courriel && courriel.length > 255) || (po_client && po_client.length > 64) ||
-            (vendeur && vendeur.length > 4) || (commentaire && commentaire.length > 512) || (attention && attention.length > 255)) {
-            res.status(400).json({ "erreur": "Données invalides", "code": 5 });
-        }
-        /**
-         * Validation adresse courriel valide
-         * @see https://www.w3resource.com/javascript/form/email-validation.php
-         */
-        if (!(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(courriel))) {
-            res.status(400).json({ "erreur": "Données invalides", "code": 5 });
-        }
         // Requête
-        else {
+        if (await validateCommande(entreprise, nom, no_compte, telephone, courriel, po_client, vendeur, commentaire, attention) && await validateId(id)) {
             const modCommande = await pool.query(
                 "UPDATE commandes SET entreprise = $1, nom = $2, no_compte = $3, telephone = $4, courriel = $5, po_client = $6, vendeur = $7, commentaire = $8, attention = $9, date_modification = CURRENT_TIMESTAMP WHERE id = $10 RETURNING *",
                 [entreprise, nom, no_compte, telephone, courriel, po_client, vendeur, commentaire, attention, id]
@@ -170,7 +224,9 @@ router.put('/:id', async (req, res) => {
             res.json([modCommande.rows, modEtat.rows]);
             sendMail(attention, "Modification d'une commande", "Une commande qui vous est assignée dans l\'application Sidekick a été modifiée.");
             console.log("Success PUT.");
-
+            // Ereur
+        } else {
+            res.status(400).json({ "erreur": "Données invalides" });
         }
     } catch (error) {
         console.log(error.message);
@@ -187,6 +243,7 @@ router.delete('/:id', async (req, res) => {
         res.json(supprCommande.rows);
         console.log("Success DELETE.");
         sendMail(supprCommande.rows[0].attention, "Suppression d'une commande", "Une commande qui vous était assignée dans l\'application Sidekick a été supprimée.");
+        // Erreur
     } catch (error) {
         console.log(error.message);
     }
