3a5311f8d66822005158cf75e11a83286b405169
Alexinlife <alexinlife.exe@gmail.com>
2021-02-04 16:31:54 -0500

    progression de la création de commandes

--- app/src/components/CreationCommande.jsx
+++ app/src/components/CreationCommande.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import '../css/Style.css';
 import NavBar from './rfc/NavBar';
-import { Formik, Form, useField } from 'formik';
+import CreationProduits from './rfc/CreationProduits';
+import { Formik, Form, useField, Field } from 'formik';
 import * as Yup from 'yup';
 // Composants Material-UI
 import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
 import Button from '@material-ui/core/Button';
+import { MenuItem } from '@material-ui/core';
 
 /**
  * @author Alex Lajeunesse
@@ -15,41 +20,125 @@ import Button from '@material-ui/core/Button';
  */
 export default class CreationCommande extends React.Component {
 
+    /**
+     * @author Alex Lajeunesse
+     * @constructor de CreationCommande
+     * @param {*} props Les propriétés React
+     */
     constructor(props) {
         super();
 
         // Localisation en français
         Yup.setLocale({
             mixed: {
+                // eslint-disable-next-line
                 default: 'Champ ${path} invalide',
+                // eslint-disable-next-line
                 required: 'Le champ ${path} est obligatoire'
             },
             string: {
+                // eslint-disable-next-line
                 max: 'Le champ ${path} doit contenir un maximum de ${max} caractères',
+                // eslint-disable-next-line
                 email: 'L\'adresse courriel doit être valide'
             },
             number: {
+                // eslint-disable-next-line
                 max: 'Le champ ${path} doit avoir un nombre inférieur à ${max}'
             }
         });
 
+        // Schéma de validation Yup
         this.schema = Yup.object({
             entreprise: Yup.string()
-                .max(64),
+                .max(64)
+                /**
+                 * @see https://krzysztofzuraw.com/blog/2020/yup-validation-two-fields
+                 */
+                .test(
+                    'ouInclusifEntrNom', // test name
+                    'Au moins l\'un des champs entreprise et nom doit être rempli', // validation message to the user
+                    // it has to be function definition to use 'this'
+                    function (entreprise) {
+                        const { nom } = this.parent;
+                        if (entreprise || nom) {
+                            return true; // lorsqu'il y a au moins un champs de rempli
+                        }
+                        return false;
+                    }
+                )
+                .test(
+                    'ouInclusifEntrTelCourriel', // test name
+                    'Le client n\'a présentement aucune information de contact', // validation message to the user
+                    // it has to be function definition to use 'this'
+                    function (entreprise) {
+                        const { telephone, courriel } = this.parent;
+                        if (entreprise || telephone || courriel) {
+                            return true; // lorsqu'il y a au moins un champs de rempli
+                        }
+                        return false;
+                    }
+                ),
 
             nom: Yup.string()
-                .max(64),
+                .max(64)
+                /**
+                 * @see https://krzysztofzuraw.com/blog/2020/yup-validation-two-fields
+                 */
+                .test(
+                    'ouInclusifEntrNom', // test name
+                    'Au moins l\'un des champs entreprise et nom doit être rempli', // validation message to the user
+                    // it has to be function definition to use 'this'
+                    function (nom) {
+                        const { entreprise } = this.parent;
+                        if (entreprise || nom) {
+                            return true; // lorsqu'il y a au moins un champs de rempli
+                        }
+                        return false;
+                    }
+                ),
 
             no_compte: Yup.number()
                 .max(169999999)
+                // eslint-disable-next-line
                 .typeError('Le champs ${path} doit contenir un nombre'),
 
             telephone: Yup.string()
-                .max(32),
+                .max(32)
+                /**
+                 * @see https://krzysztofzuraw.com/blog/2020/yup-validation-two-fields
+                 */
+                .test(
+                    'ouInclusifEntrTelCourriel', // test name
+                    'Le client n\'a présentement aucune information de contact', // validation message to the user
+                    // it has to be function definition to use 'this'
+                    function (telephone) {
+                        const { entreprise, courriel } = this.parent;
+                        if (entreprise || telephone || courriel) {
+                            return true; // lorsqu'il y a au moins un champs de rempli
+                        }
+                        return false;
+                    }
+                ),
 
             courriel: Yup.string()
                 .email()
-                .max(255),
+                .max(255)
+                /**
+                 * @see https://krzysztofzuraw.com/blog/2020/yup-validation-two-fields
+                 */
+                .test(
+                    'ouInclusifEntrTelCourriel', // test name
+                    'Le client n\'a présentement aucune information de contact', // validation message to the user
+                    // it has to be function definition to use 'this'
+                    function (courriel) {
+                        const { entreprise, telephone } = this.parent;
+                        if (entreprise || telephone || courriel) {
+                            return true; // lorsqu'il y a au moins un champs de rempli
+                        }
+                        return false;
+                    }
+                ),
 
             po_client: Yup.string()
                 .max(64),
@@ -61,10 +150,11 @@ export default class CreationCommande extends React.Component {
             commentaire: Yup.string()
                 .max(512),
 
+            // Présentement inutile avec le select mis en place
             attention: Yup.string()
                 .email()
                 .max(255)
-                .required()
+                .required(),
         });
     }
 
@@ -72,7 +162,8 @@ export default class CreationCommande extends React.Component {
      * @author Alex Lajeunesse
      * @function render
      * @description Affiche l'interface.
-     * @see https://www.youtube.com/watch?v=FD50LPJ6bjE
+     * @see https://www.youtube.com/watch?v=FD50LPJ6bjE pour Formik et MyTextField
+     * @see https://formik.org/docs/examples/field-arrays pour les produits
      */
     render() {
         const MyTextField = ({ placeholder, ...props }) => {
@@ -95,88 +186,78 @@ export default class CreationCommande extends React.Component {
                 </header>
                 <div className="content">
                     <h1>Créer une commande</h1>
-                    <Paper>
-                        <Formik
-                            validateOnChange
-                            initialValues={{ entreprise: '', nom: '', no_compte: '', telephone: '', courriel: '', po_client: '', vendeur: '', commentaire: '', attention: '' }}
-                            onSubmit={(data, { setSubmitting }) => {
-                                setSubmitting(true);
-                                console.log(data);
-                                setSubmitting(false);
-                            }}
-                            validationSchema={this.schema}
-                        >
-                            {({ values, isSubmitting, handleChange, handleBlur, handleSubmit }) => (
-                                <Form>
+                    <Formik
+                        validateOnChange
+                        initialValues={{ entreprise: '', nom: '', no_compte: '', telephone: '', courriel: '', po_client: '', vendeur: '', commentaire: '', attention: '' }}
+                        onSubmit={(data, { setSubmitting }) => {
+                            setSubmitting(true);
+                            console.log(data);
+                            setSubmitting(false);
+                        }}
+                        validationSchema={this.schema}
+                    >
+                        {({ values, isSubmitting, handleChange, handleBlur, handleSubmit }) => (
+                            <Form>
+                                <Paper>
                                     <div>
                                         <MyTextField
                                             placeholder="Nom d'entreprise"
                                             name="entreprise"
-                                            type="input"
-                                            as={TextField}
                                         />
                                         <MyTextField
                                             placeholder="Numéro de compte"
                                             name="no_compte"
-                                            type="input"
-                                            as={TextField}
                                         />
                                         <MyTextField
                                             placeholder="Nom du client"
                                             name="nom"
-                                            type="input"
-                                            as={TextField}
                                         />
                                     </div>
                                     <div>
                                         <MyTextField
                                             placeholder="Téléphone"
                                             name="telephone"
-                                            type="input"
-                                            as={TextField}
                                         />
                                         <MyTextField
                                             placeholder="Courriel"
                                             name="courriel"
-                                            type="input"
-                                            as={TextField}
                                         />
                                         <MyTextField
                                             placeholder="PO du client"
                                             name="po_client"
-                                            type="input"
-                                            as={TextField}
                                         />
                                     </div>
                                     <div>
                                         <MyTextField
                                             placeholder="*Vendeur"
                                             name="vendeur"
-                                            type="input"
-                                            as={TextField}
                                         />
-                                        <MyTextField
-                                            placeholder="*Attention"
-                                            name="attention"
-                                            type="input"
-                                            as={TextField}
-                                        />
+                                        <FormControl className="form-item">
+                                            <InputLabel htmlFor="attention">*Attention</InputLabel>
+                                            <Field
+                                                type="select"
+                                                as={Select}
+                                                name="attention"
+                                            >
+                                                <MenuItem value={'alexinlife.exe@gmail.com'}>Alex Lajeunesse</MenuItem>
+                                            </Field>
+                                        </FormControl>
                                     </div>
                                     <div>
                                         <MyTextField
                                             placeholder="Commentaire"
                                             name="commentaire"
-                                            type="input"
-                                            as={TextField}
                                         />
                                     </div>
                                     <div>
                                         <Button className="form-item" disabled={isSubmitting} type="submit"><b>Enregistrer</b></Button>
                                     </div>
-                                </Form>
-                            )}
-                        </Formik>
-                    </Paper>
+                                    <pre>{JSON.stringify(values, null, 2)}</pre>
+                                </Paper>
+                            </Form>
+                        )}
+                    </Formik>
+                    <CreationProduits />
                 </div>
             </div>
         )
---
+++ app/src/components/rfc/CreationProduits.jsx
@@ -0,0 +1,182 @@
+import React from 'react';
+import '../../css/Style.css';
+import { Formik, Form, FieldArray, useField } from 'formik';
+import * as Yup from 'yup';
+// Composants Material-UI
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+const initialValues = {
+    produits: [
+        {
+            code: '',
+            descrption: '',
+            qte_demandee: '',
+            prix: ''
+        },
+    ],
+};
+
+// Localisation en français
+Yup.setLocale({
+    mixed: {
+        // eslint-disable-next-line
+        default: 'Champ ${path} invalide',
+        // eslint-disable-next-line
+        required: 'Le champ ${path} est obligatoire'
+    },
+    string: {
+        // eslint-disable-next-line
+        max: 'Le champ ${path} doit contenir un maximum de ${max} caractères',
+    },
+    number: {
+        // eslint-disable-next-line
+        integer: 'Le champ ${path} doit contenir un nombre entier',
+        // eslint-disable-next-line
+        positive: 'Le champ ${path} doit contenir un nombre positif',
+        // eslint-disable-next-line
+        min: 'Le champ ${path} doit avoir un nombre supérieur à ${min}'
+    }
+});
+
+// Schéma de validation Yup
+const schema = Yup.object({
+    produits: Yup.array().of(
+        Yup.object({
+            code: Yup.string()
+                .max(32)
+                /**
+                 * @see https://krzysztofzuraw.com/blog/2020/yup-validation-two-fields
+                 */
+                .test(
+                    'ouInclusifCodeDesc', // test name
+                    'Au moins l\'un des champs code et description doit être rempli', // validation message to the user
+                    // it has to be function definition to use 'this'
+                    function (code) {
+                        const { description } = this.parent;
+                        if (code || description) {
+                            return true; // lorsqu'il y a au moins un champs de rempli
+                        }
+                        return false;
+                    }
+                ),
+
+            description: Yup.string()
+                .max(255)
+                /**
+                 * @see https://krzysztofzuraw.com/blog/2020/yup-validation-two-fields
+                 */
+                .test(
+                    'ouInclusifCodeDesc', // test name
+                    'Au moins l\'un des champs code et description doit être rempli', // validation message to the user
+                    // it has to be function definition to use 'this'
+                    function (description) {
+                        const { code } = this.parent;
+                        if (code || description) {
+                            return true; // lorsqu'il y a au moins un champs de rempli
+                        }
+                        return false;
+                    }
+                ),
+
+            qte_demandee: Yup.number()
+                .integer()
+                .min(1)
+                .required()
+                // eslint-disable-next-line
+                .typeError('Le champs ${path} doit contenir un nombre'),
+
+            prix: Yup.number()
+                .min(0.0050)
+                .positive()
+                // eslint-disable-next-line
+                .typeError('Le champs ${path} doit contenir un nombre')
+        })
+    )
+});
+
+const MyTextField = ({ placeholder, ...props }) => {
+    const [field, meta] = useField(props);
+    const error = meta.error && meta.touched ? meta.error : '';
+    return (
+        <TextField
+            className="product-form-item"
+            placeholder={placeholder}
+            {...field}
+            helperText={error}
+            error={!!error}
+        />
+    );
+}
+
+/**
+ * @author Alex Lajeunesse
+ * @function CréerProduits
+ * @description Gère la section d'ajout de produits (affichage, requêtes à l'API, redirection et formulaires)
+ * @see https://formik.org/docs/examples/field-arrays
+ */
+export default function CreerProduits() {
+    return (
+        <div>
+            <h1>Produits</h1>
+            <Paper>
+                <Formik
+                    validateOnChange
+                    initialValues={initialValues}
+                    onSubmit={(data, { setSubmitting }) => {
+                        setSubmitting(true);
+                        console.log(data);
+                        setSubmitting(false);
+                    }}
+                    validationSchema={schema}
+                >
+                    {({ values, isSubmitting, handleChange, handleBlur, handleSubmit }) => (
+                        <Form>
+                            <FieldArray name="produits">
+                                {({ insert, remove, push }) => (
+                                    <div>
+                                        {values.produits.length > 0 &&
+                                            values.produits.map((produit, index) => (
+                                                <div className="row" key={index}>
+                                                    <div className="col">
+                                                        <MyTextField
+                                                            name={`produits.${index}.qte_demandee`}
+                                                            placeholder="Quantité demandée"
+                                                            type="text"
+                                                        />
+                                                        <MyTextField
+                                                            name={`produits.${index}.code`}
+                                                            placeholder="Code"
+                                                            type="text"
+                                                        />
+                                                        <MyTextField
+                                                            name={`produits.${index}.description`}
+                                                            placeholder="Description"
+                                                            type="text"
+                                                        />
+                                                        <MyTextField
+                                                            name={`produits.${index}.prix`}
+                                                            placeholder="Prix"
+                                                            type="text"
+                                                        />
+                                                        <Button className="form-button" disabled={isSubmitting} onClick={() => remove(index)}><b>X</b></Button>
+                                                    </div>
+                                                    <div className="col">
+                                                    </div>
+                                                </div>
+                                            ))}
+                                        <Button className="form-item" disabled={isSubmitting}
+                                            onClick={() => push({ code: '', descrption: '', qte_demandee: '', prix: '' })}
+                                        ><b>Ajouter un produit</b></Button>
+                                    </div>
+                                )}
+                            </FieldArray>
+                            <Button className="form-item" disabled={isSubmitting} type="submit"><b>Enregistrer</b></Button>
+                        </Form>
+                    )}
+                </Formik>
+            </Paper>
+        </div>
+    );
+}
